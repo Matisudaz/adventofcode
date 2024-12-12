e66a00ccf94563cc6e096f42e43a952073991bbc
Matisudaz <rango.he@outlook.com>
2024-12-12 17:01:03 +0800

    complete day2

--- 2024/day2.lisp
+++ 2024/day2.lisp
@@ -19,18 +19,58 @@
              (decrease '(> prev cur)))
           (< (abs (- prev cur)) 4))))
 
-(defun safe-report-p (report)
-  "Check a report is safe or not."
-  (labels ((local-helper (r prev p)
-             (if (null r)
-                 t
-                 (and (funcall p prev (car r)) (local-helper (cdr r) (car r) p)))))
-    (cond
-      ((or (null report) (null (cdr report))) t)
-      ((> (car report) (cadr report))
-       (local-helper (cdr report) (car report) (create-check-safe-report :decrease t)))
-      (t (local-helper (cdr report) (car report) (create-check-safe-report :increase t))))))
 
+(defun safe-report-p (report &optional (tolerate-bad-level 0))
+  "Check a report is safe or not."
+  (labels
+      ((local-helper-1 (report process-list prev p tolerate-bad-level)
+         (cond
+           ((null report) t)
+           ((funcall p prev (car report))
+            (local-helper-1
+             (cdr report)
+             (cons prev process-list)
+             (car report)
+             p
+             tolerate-bad-level))
+           ((> tolerate-bad-level 0)
+            (cond
+              ((null process-list)
+               (or
+                (local-helper-2 report (- tolerate-bad-level 1))
+                (local-helper-2 (cons prev (cdr report)) (- tolerate-bad-level 1))))
+              ((null (cdr process-list))
+               (or
+                (local-helper-2 (cons prev report) (- tolerate-bad-level 1))
+                (local-helper-2 (cons (car process-list) report) (- tolerate-bad-level 1))
+                (local-helper-2 (append process-list (list prev) (cdr report)) (- tolerate-bad-level 1))))
+              (t
+               (or
+                (local-helper-1
+                 report
+                 (cdr process-list)
+                 (car process-list)
+                 p
+                 (- tolerate-bad-level 1))
+                (local-helper-1
+                 (cdr report)
+                 process-list
+                 prev
+                 p
+                 (- tolerate-bad-level 1))))))
+           (t nil)))
+       (local-helper-2 (report tolerate-bad-level)
+         (cond
+           ((or (null report) (null (cdr report))) t)
+           (t (local-helper-1
+               (cdr report)
+               nil
+               (car report)
+               (if (> (car report) (cadr report))
+                   (create-check-safe-report :decrease t)
+                   (create-check-safe-report :increase t))
+               tolerate-bad-level)))))
+    (local-helper-2 report tolerate-bad-level)))
 
 (defun read-numbers-list-from-stream (stream)
   "Returns two list from a stream."
@@ -46,5 +86,12 @@
   (let ((result 0))
     (loop for lst in (read-numbers-list-from-file file)
           when (safe-report-p lst)
+            do (incf result))
+    result))
+
+(defun calc-safe-report-with-tolerate-single-bad-level-from-file (file)
+  (let ((result 0))
+    (loop for lst in (read-numbers-list-from-file file)
+          when (safe-report-p lst 1)
             do (incf result))
     result))
